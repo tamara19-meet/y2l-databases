908295e3c47c6f1cfbe387a0f475d10c4fa19ea3
aboud20-meet <aboud20@meet.mit.edu>
2018-11-01 18:49:26 +0200

    ehkfe

--- databases.py
+++ databases.py
@@ -10,16 +10,29 @@ session = DBSession()
 
 # Write your functions to interact with the database here :
 
-def create_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+def create_product(name, Comes_with, Type_of_celebration, price):
+	new_object = Product(
+		name=name,
+		Comes_with=Comes_with,
+		Type_of_celebration=Type_of_celebration,
+		price=price)
+	session.add(new_object)
+	session.commit()
 
-def update_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+def update_product(price, Comes_with):
+	new_object=session.query(Product).filter_by(
+		price=price).first()
+	new_object.Comes_with=Comes_with
+	session.commit()
 
-def delete_product(id):
-  pass
+def delete_product(name):
+	session.query(Product).filter_by(
+		name=name).delete()
+	session.commit()
 
 def get_product(id):
   pass
+create_product("SUPER EYELASH GLUE", "2 pairs of lashes", "BROTHERS WEDDING", 300)
+create_product("NORMAL EYELASH GLUE", "3 pairs of lashes", "UNCLES WEDDING", 400)
+delete_product("SUPER EYELASH GLUE")
+update_product(800, "10 EYELASHES")
--- model.py
+++ model.py
@@ -7,5 +7,9 @@ Base = declarative_base()
 
 # Write your classes here :
 class Product(Base):
-    # TODO: complete this class
-    pass
+	__tablename__ = 'EYELASH GLUE'
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	Comes_with= Column(String)
+	Type_of_celebration= Column(String)
+	price=Column(Integer)
